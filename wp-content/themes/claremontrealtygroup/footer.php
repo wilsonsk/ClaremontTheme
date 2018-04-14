bd36ffded36e8a7a2673a48625e22060d3a90bb2
Sky <wilson.skylerm@gmail.com>
2018-04-14 11:33:58 -0700

    update javasript in footer

--- wp-content/themes/claremontrealtygroup/footer.php
+++ wp-content/themes/claremontrealtygroup/footer.php
@@ -11,26 +11,20 @@
 
 ?>
 
-	</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'claremontrealtygroup' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'claremontrealtygroup' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'claremontrealtygroup' ), 'claremontrealtygroup', '<a href="http://underscores.me/">Sky Wilson</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
 
 <?php wp_footer(); ?>
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="<?php bloginfo('template_directory') ?>/assets/js/jquery-2.1.1.min.js"></script>
+<script src="<?php bloginfo('template_directory') ?>/assets/js/bootstrap.min.js"></script>
+<script src="<?php bloginfo('template_directory') ?>/assets/js/main.js"></script>
+
+<script type="text/javascript" src="//use.typekit.net/gla7wnd.js"></script>
+<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
 
 </body>
 </html>
