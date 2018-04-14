e76fb533e9bae14a5fde4979b34d82b8b4174992
Sky <wilson.skylerm@gmail.com>
2018-04-14 08:05:02 -0700

    update nav menu in functions.php

--- wp-content/themes/claremontrealtygroup/functions.php
+++ wp-content/themes/claremontrealtygroup/functions.php
@@ -44,7 +44,7 @@ if ( ! function_exists( 'claremontrealtygroup_setup' ) ) :
 
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus( array(
-			'menu-1' => esc_html__( 'Primary', 'claremontrealtygroup' ),
+			'primary' => esc_html__( 'Primary', 'claremontrealtygroup' ),
 		) );
 
 		/*
